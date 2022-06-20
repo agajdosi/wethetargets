1b4c53d9da86bb12c516d9e66ed13913b7efbf80
agajdosi <andreas.gajdosik@gmail.com>
2022-06-20 17:16:58 +0200

    Init data upload to remote server

--- source/background.js
+++ source/background.js
@@ -1,6 +1,8 @@
 let portFromContentScript;
 let portFromPopup;
 
+const APP_ID = 77
+const SERVER = "http://localhost:8888"
 let questions = [];
 let results = [];
 
@@ -25,6 +27,7 @@ function handleMessageFromContentScript(message){
 function handleMessageFromGoogle(message){
   if (message.type == "results") {
     results.push([message.question, message.results])
+    uploadGoogleResults(message.question, message.results)
     return
   }
   
@@ -42,9 +45,26 @@ function handleMessageFromPopup(message){
   if (message.command == "start the google test") runGoogleTest();
 }
 
-
 function runGoogleTest(){
   results = [];
   questions = ["how to", "where to", "which"];
   browser.tabs.update({url: "https://google.com"})
+}
+
+function uploadGoogleResults(question, results){
+  let api_endpoint = SERVER + "/browser-plugin"
+  let data = {
+    "app_id": APP_ID,
+    "person_info": null,
+    "question": question,
+    "answers": results,
+  }
+  fetch(api_endpoint, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
 }
