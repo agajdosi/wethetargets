6404b2eb0725252c11d0ebf301ce9172d527919c
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2023-03-02 17:16:45 +0100

    Upload suggestions to function and MongoDB

--- source/background/bg.js
+++ source/background/bg.js
@@ -1,8 +1,8 @@
 let portFromContentScript;
 let portFromPopup;
 
-const APP_ID = 77;
-const SERVER = 'http://localhost:8888';
+const EXTENSION_VERSION = '0.0.2';
+const SERVER = 'https://artops.gajdosik.org/wethetargets';
 let questions = [];
 let results = [];
 
@@ -20,41 +20,7 @@ function connected(port) {
 	}
 }
 
-function handleMessageFromContentScript(message) {
-	if (message.page === 'google') {
-		handleMessageFromGoogle(message);
-	}
-}
-
-function handleMessageFromGoogle(message) {
-	if (message.type === 'results-suggestedSearches') {
-		console.log('results-suggestedSearches:', message.results);
-		results.push([message.question, message.results]);
-		uploadGoogleResults(message.question, message.results);
-		return;
-	}
-
-	if (message.type === 'results-ofSearch') {
-		console.log('results-ofSearch:', message.results);
-	}
-
-	if (message.type === 'loaded') {
-		if (message.pathname === '/search') {
-			return;
-		}
-
-		if (message.pathname === '/') {
-			const question = questions.pop();
-			if (question === undefined) {
-				console.log(results);
-				return;
-			}
-
-			portFromContentScript.postMessage({question});
-		}
-	}
-}
-
+// POPUP communication
 function handleMessageFromPopup(message) {
 	if (message.command === 'start the google test') {
 		runGoogleTest();
@@ -67,22 +33,64 @@ function runGoogleTest() {
 	browser.tabs.update({url: 'https://www.google.com'});
 }
 
-function uploadGoogleResults(question, results) {
-	const apiEndpoint = SERVER + '/browser-plugin';
+// CONTENT SCRIPT communication
+function handleMessageFromContentScript(message) {
+	if (message.page === 'google') {
+		handleMessageFromGoogle(message);
+	}
+}
+
+// GOOGLE CONTENT
+function handleMessageFromGoogle(message) {
+	if (message.type === 'results-suggestedSearches') {
+		console.log('results-suggestedSearches:', message.results);
+		results.push([message.question, message.results]);
+		uploadGoogleSuggestions(message.question, message.results);
+		return;
+	}
+
+	if (message.type === 'results-ofSearch') {
+		console.log('results-ofSearch:', message.results);
+	}
+
+	// Page loaded
+	if (message.type === 'loaded') {
+		if (message.pathname === '/search') {
+			return;
+		}
+
+		// Google home page was loaded
+		if (message.pathname === '/') {
+			const question = questions.pop();
+			if (question === undefined) {
+				console.log(results);
+				return;
+			}
+
+			portFromContentScript.postMessage({question});
+		}
+	}
+}
+
+async function uploadGoogleSuggestions(question, suggestions) {
+	const apiEndpoint = SERVER + '/ggl-suggestions';
 	const data = {
-		appID: APP_ID,
-		personInfo: null,
+		extensionVersion: EXTENSION_VERSION,
+		demographics: {born: 1992, sex: 'male', country: 'CZ'}, // TODO: get actual data from popup
 		question,
-		answers: results,
+		suggestions,
 	};
-	fetch(apiEndpoint, {
+	const options = {
 		method: 'POST',
 		headers: {
 			Accept: 'application/json, text/plain, */*',
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify(data),
-	});
+	};
+	const response = await fetch(apiEndpoint, options);
+	const results = await response.json();
+	console.log('uploadGoogleSuggestions results:', results);
 }
 
 function getResearchQuestions() {
